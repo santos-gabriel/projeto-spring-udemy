434ed94582feed192f4b17a48614c5db92ff2942
gabriel <gabrielalmeidads@gmail.com>
2020-07-24 21:56:36 -0300

    Adicionando arquivos

---
+++ src/main/java/com/project/course/ProjectCourseApplication.java
@@ -0,0 +1,13 @@
+package com.project.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCourseApplication.class, args);
+	}
+
+}
